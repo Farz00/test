59f01d409d50390d2c0d616d054952868fd9c01f
farz <farizuan.akmal2@gmail.com>
2022-11-07 22:29:17 -0800

    class activity

---
+++ boolean.py
@@ -0,0 +1,13 @@
+first_int = int(input("Enter value for 1st Int: "))
+second_int = int(input("Enter value for 2nd Int: "))
+
+
+if first_int > second_int:
+	print("The first int is bigger!")
+	
+if first_int == second_int:
+	print("both number are equal!")
+	
+else: 
+	print("The second_int is bigger!")
+
